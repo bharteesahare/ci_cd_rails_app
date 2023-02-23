0edd492962acceea3c53f031e40664803243e8cd
bhartee-sahare <bhartee@vebuin.com>
2023-02-23 12:54:43 +0530

    add the model test and validation

---
+++ test/models/post_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should not save the post the without title" do
+    post = Post.new
+    assert_not post.save, "Saved the post without title"
+  end
+
+  test "should save the post with title" do
+    post = Post.new(title: "test the title")
+    assert post.save, "save the post successfully"
+  end
+end
